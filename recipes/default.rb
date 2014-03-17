9387e3afb4c165992c45454b4df783776cc27513
Jason Boyles <jason@duncancreek.net>
2014-03-17 17:42:56 -0500

    hard code eth2 interface for bind

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,11 +15,6 @@ if node[:rax_serf][:raw_encrypt_key]
     Base64.encode64(node[:rax_serf][:raw_encrypt_key]).chop
 end
 
-node[:network][:interfaces][:eth2][:addresses].each_pair do |addr,info|
-  if info[:family] == "inet"
-    node.set[:serf][:agent][:bind] = addr
-    break
-  end
-end
+node.set[:serf][:agent][:interface] = 'eth2'
 
 include_recipe 'serf::default'
