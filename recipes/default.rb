ad0b1cd8d3cacd7badd7bc8a5fd3b4ff68c68225
Jason Boyles <jason@duncancreek.net>
2014-03-17 17:30:01 -0500

    set serf bind address to the network on eth2

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,4 +15,11 @@ if node[:rax_serf][:raw_encrypt_key]
     Base64.encode64(node[:rax_serf][:raw_encrypt_key]).chop
 end
 
+node[:network][:interfaces][:eth2][:addresses].each_pair do |addr,info|
+  if info[:family] == "inet"
+    node.set[:serf][:agent][:bind] = addr
+    break
+  end
+end
+
 include_recipe 'serf::default'
