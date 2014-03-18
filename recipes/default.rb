f08e933e2730412555aa024f4d962e8d622d8f74
Jason Boyles <jason@duncancreek.net>
2014-03-18 10:32:57 -0500

    add logging messages

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,8 +19,11 @@ node.set[:serf][:version] = '0.5.0'
 node.set[:serf][:agent][:interface] = 'eth2'
 
 node[:network][:interfaces][:eth0][:addresses].each_pair do |addr,info|
+  Chef::Log.info("evaluating address #{addr}")
   if info[:family] == 'inet'
+    Chef::Log.info("looks like #{addr} is inet")
     node.set[:serf][:node_name] = node[:hostname] + "-" + addr
+    Chef::Log.info("set node_name to #{node[:serf][:node_name]}")
     break
   end
 end
